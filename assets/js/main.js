8a6a41b99c136ff24128b4974903844e0b3e0258
Pavan <vattyampv@gmail.com>
2017-10-27 15:22:33 -0400

    Changed everthing``
    Changed the template

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,93 +1,127 @@
-/*
-	ZeroFour by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+$(window).load(function () { // makes sure the whole site is loaded
+    $('#status').fadeOut(); // will first fade out the loading animation
+    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+    $('body').delay(350).css({'overflow': 'visible'});
+})
+$(document).ready(function () {
 
-(function($) {
+    
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_square-yellow',
+        radioClass: 'iradio_square-yellow',
+        increaseArea: '20%' // optional
+    });
 
-	skel
-		.breakpoints({
-			desktop: '(min-width: 737px)',
-			tablet: '(min-width: 737px) and (max-width: 1200px)',
-			mobile: '(max-width: 736px)'
-		})
-		.viewport({
-			breakpoints: {
-				tablet: {
-					width: 1080
-				}
-			}
-		});
 
-	$(function() {
+    $('.layout-grid').on('click', function () {
+        $('.layout-grid').addClass('active');
+        $('.layout-list').removeClass('active');
 
-		var	$window = $(window),
-			$body = $('body');
+        $('#list-type').removeClass('proerty-th-list');
+        $('#list-type').addClass('proerty-th');
 
-		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+    });
 
-			$window.on('load', function() {
-				$body.removeClass('is-loading');
-			});
+    $('.layout-list').on('click', function () {
+        $('.layout-grid').removeClass('active');
+        $('.layout-list').addClass('active');
 
-		// Fix: Placeholder polyfill.
-			$('form').placeholder();
+        $('#list-type').addClass('proerty-th-list');
+        $('#list-type').removeClass('proerty-th');
 
-		// Dropdowns.
-			$('#nav > ul').dropotron({
-				offsetY: -22,
-				mode: 'fade',
-				noOpenerFade: true,
-				speed: 300,
-				detach: false
-			});
+    });
 
-		// Prioritize "important" elements on mobile.
-			skel.on('+mobile -mobile', function() {
-				$.prioritize(
-					'.important\\28 mobile\\29',
-					skel.breakpoint('mobile').active
-				);
-			});
+});
+$(document).ready(function () {
+    $("#bg-slider").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        autoPlay: 5000,
+        paginationSpeed: 100,
+        singleItem: true,
+        mouseDrag: false,
+        transitionStyle: "fade"
+                // "singleItem:true" is a shortcut for:
+                // items : 1, 
+                // itemsDesktop : false,
+                // itemsDesktopSmall : false,
+                // itemsTablet: false,
+                // itemsMobile : false 
+    });
+    $("#prop-smlr-slide_0").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        pagination: true,
+        paginationSpeed: 100,
+        items: 3
 
-		// Off-Canvas Navigation.
+    });
+    $("#testimonial-slider").owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 100,
+        pagination: true,
+        paginationSpeed: 100,
+        items: 3
+    });
 
-			// Title Bar.
-				$(
-					'<div id="titleBar">' +
-						'<a href="#navPanel" class="toggle"></a>' +
-						'<span class="title">' + $('#logo').html() + '</span>' +
-					'</div>'
-				)
-					.appendTo($body);
+    $('#price-range').slider();
+    $('#property-geo').slider();
+    $('#min-baths').slider();
+    $('#min-bed').slider();
 
-			// Navigation Panel.
-				$(
-					'<div id="navPanel">' +
-						'<nav>' +
-							$('#nav').navList() +
-						'</nav>' +
-					'</div>'
-				)
-					.appendTo($body)
-					.panel({
-						delay: 500,
-						hideOnClick: true,
-						hideOnSwipe: true,
-						resetScroll: true,
-						resetForms: true,
-						side: 'left',
-						target: $body,
-						visibleClass: 'navPanel-visible'
-					});
+    var RGBChange = function () {
+        $('#RGB').css('background', '#FDC600')
+    };
 
-			// Fix: Remove navPanel transitions on WP<10 (poor/buggy performance).
-				if (skel.vars.os == 'wp' && skel.vars.osVersion < 10)
-					$('#titleBar, #navPanel, #page-wrapper')
-						.css('transition', 'none');
+    // Advanced search toggle
+    var $SearchToggle = $('.search-form .search-toggle');
+    $SearchToggle.hide();
 
-	});
+    $('.search-form .toggle-btn').on('click', function (e) {
+        e.preventDefault();
+        $SearchToggle.slideToggle(300);
+    });
 
-})(jQuery);
+    setTimeout(function () {
+        $('#counter').text('0');
+        $('#counter1').text('0');
+        $('#counter2').text('0');
+        $('#counter3').text('0');
+        setInterval(function () {
+            var curval = parseInt($('#counter').text());
+            var curval1 = parseInt($('#counter1').text().replace(' ', ''));
+            var curval2 = parseInt($('#counter2').text());
+            var curval3 = parseInt($('#counter3').text());
+            if (curval <= 1007) {
+                $('#counter').text(curval + 1);
+            }
+            if (curval1 <= 1280) {
+                $('#counter1').text(sdf_FTS((curval1 + 20), 0, ' '));
+            }
+            if (curval2 <= 145) {
+                $('#counter2').text(curval2 + 1);
+            }
+            if (curval3 <= 1022) {
+                $('#counter3').text(curval3 + 1);
+            }
+        }, 2);
+    }, 500);
+
+    function sdf_FTS(_number, _decimal, _separator) {
+        var decimal = (typeof (_decimal) != 'undefined') ? _decimal : 2;
+        var separator = (typeof (_separator) != 'undefined') ? _separator : '';
+        var r = parseFloat(_number)
+        var exp10 = Math.pow(10, decimal);
+        r = Math.round(r * exp10) / exp10;
+        rr = Number(r).toFixed(decimal).toString().split('.');
+        b = rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g, "\$1" + separator);
+        r = (rr[1] ? b + '.' + rr[1] : b);
+
+        return r;
+    }
+
+})
+
+// Initializing WOW.JS
+
+new WOW().init();
