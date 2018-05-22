7d5f8da1deceec2f9b34afafdbd7b776d4b040b4
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-22 15:27:07 -0400

    Add array printing functionality

--- assets/js/adloader.js
+++ assets/js/adloader.js
@@ -6,3 +6,13 @@ arrImg = ["assets/img/Mason-oh.jpg", "assets/img/Mason-oh.jpg", "assets/img/Maso
 arrAlt = ["Afidence", "Comey & Sheperd – Jessica Mills", "Eco Development Group"];
 arrHeader = arrAlt;
 arrDescription = ["Providing unbiased, real-time business and IT solutions.", "Brokering fine home since 1946.", "Delivering efficient, sustainable energy related solutions nation-wide."];
+
+for (var i = 1; i < numAds; i++) {
+    document.write("<div " + outerDiv + ">");
+    document.write('<img src="' + arrImg[i] + '"' + ' alt = "' + arrAlt[i] + '">');
+    document.write("<div " + innerDiv + ">");
+    document.write("<h3>" + arrHeader[i] + "</h3>");
+    document.write("<p>" + arrDescription[i] + "</p>");
+    document.write("</div>");
+    document.write("</div>");
+}
