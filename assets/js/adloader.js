b90e7970b13c039155c75a364d60a2411f4889bb
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-22 15:12:56 -0400

    Add arrays

--- assets/js/adloader.js
+++ assets/js/adloader.js
@@ -1,1 +1,4 @@
-
+arrImg = ["assets/img/Mason-oh.jpg", "assets/img/Mason-oh.jpg", "assets/img/Mason-oh.jpg"];
+arrAlt = ["Afidence", "Comey & Sheperd – Jessica Mills", "Eco Development Group"];
+arrHeader = arrAlt;
+arrDescription = ["Since 2010, we have been providing our clients with unbiased, real-time business and IT solutions. We are a company built on trust.", "Brokering fine home since 1946.", "delivers efficient, sustainable energy related solutions nation-wide"];
