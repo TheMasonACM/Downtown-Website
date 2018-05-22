b856a7f8433b20ca35742656c8be6eb87b49d028
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-05-22 16:33:38 -0400

    Test array lengths before outputting

--- assets/js/adloader.js
+++ assets/js/adloader.js
@@ -8,12 +8,18 @@ arrAlt = ["Afidence", "Comey & Sheperd – Jessica Mills", "Eco Development Grou
 arrHeader = arrAlt;
 arrDescription = ["Providing unbiased, real-time business and IT solutions.", "Brokering fine home since 1946.", "Delivering efficient, sustainable energy related solutions nation-wide."];
 
-for (var i = 0; i < numAds; i++) {
-    document.write("<div " + ((i == 0) ? outerDivActive : outerDiv) + ">");
-    document.write('<img src="' + arrImg[i] + '"' + ' alt = "' + arrAlt[i] + '">');
-    document.write("<div " + innerDiv + ">");
-    document.write("<h3>" + arrHeader[i] + "</h3>");
-    document.write("<p>" + arrDescription[i] + "</p>");
-    document.write("</div>");
-    document.write("</div>");
+if (arrImg.length == arrAlt.length && arrAlt.length == arrDescription.length) {
+    for (var i = 0; i < numAds; i++) {
+        document.write("<div " + ((i == 0) ? outerDivActive : outerDiv) + ">");
+        document.write('<img src="' + arrImg[i] + '"' + ' alt = "' + arrAlt[i] + '">');
+        document.write("<div " + innerDiv + ">");
+        document.write("<h3>" + arrHeader[i] + "</h3>");
+        document.write("<p>" + arrDescription[i] + "</p>");
+        document.write("</div>");
+        document.write("</div>");
+    }   
 }
+else {
+    console.error("Ads will not be printed as the arrays don't match in length.");
+}
+
